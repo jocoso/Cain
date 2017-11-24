496451f9f0c1610a54bb79baa4bd0e9205a69b20
Joshua <joshua.collado022@gmail.com>
2017-11-24 08:20:51 -0500

    Adding speech recognition

--- nate.py
+++ nate.py
@@ -1,6 +1,12 @@
 import wx
 import wikipedia
 import wolframalpha
+import speech_recognition as sr
+import pyaudio as p
+
+from espeak import espeak
+
+espeak.synth("Welcome")
 
 class MyFrame(wx.Frame):
     def __init__(self):
@@ -21,21 +27,44 @@ class MyFrame(wx.Frame):
         my_sizer.Add(self.txt, 0, wx.ALL, 5)
         panel.SetSizer(my_sizer)
         self.Show()
-    
+
     def OnEnter(self, event):
         input = self.txt.GetValue()
         input = input.lower()
+
+        if input == '':
+            if p.PyAudio().get_device_count() <= 0:
+                espeak.synth("I apologize, but you don't have an available microphone.")
+            else:
+                r = sr.Recognizer()
+                with sr.Microphone() as source:
+                    audio = r.listen(source)
+                try:
+                    self.txt.SetValue(r.recognize_google(audio))
+                except sr.UnknownValueError:
+                    print("Google Speech Recognition could not understand audio")
+                except sr.RequestError as e:
+                    print("Could not request results from Google Speech Recognition service; {0}".format(e))
+
         try:
             app_id = "K7PUL4-AW9W933275"
             client = wolframalpha.Client(app_id)
             res = client.query(input)
             answer = next(res.results).text
-            print answer
+            print(answer)
+            espeak.synth("The answer is " + answer)
         except:
-            input = input.split(' ')
-            input = " ".join(input[2:])
-            print wikipedia.summary(input)
-        
+            if len(input) >= 1:
+                input = input.split(' ')
+                input = " ".join(input[2:])
+            print(len(input))
+            if input != '':
+                espeak.synth("Searched for "+input)
+                print(wikipedia.summary(input))
+            else:
+                espeak.synth("I am sorry, but you wrote nothing.")
+            #espeak.synth()
+
 if __name__ == "__main__":
     app = wx.App(True)
     frame = MyFrame()
